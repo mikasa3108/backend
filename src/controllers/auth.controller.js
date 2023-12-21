22fc41db0983d5c134fdd2548b3f196c419d7a64
Ryu <zoelabbb@gmail.com>
2023-12-21 14:00:27 +0700

    New file & Update auth.controller.js

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,102 +2,80 @@ const userModel = require('../models/users.model')
 const argon = require('argon2')
 const jwt = require("jsonwebtoken")
 
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await userModel.findOneByEmail(email);
 
-
-exports.login = async (req, res)=>{
-    try{
-        const {email, password} = req.body
-        const user = await userModel.findOneByEmail(email)
-
-        // cara standar dengan if else
-        // if(!user){
-        //     throw Error('wrong')
-        // }
-        // const verify = await argon.verify(user.password)
-        // if(verify){
-        //     return res.json({
-        //         success: true,
-        //         message: 'login success',
-        //         results: {
-        //             token : 'abc123'
-        //         }
-        //     })
-        // } else{
-        //     throw Error('wrong')
-        // }
-
-        // simpel dan rapi dengan negasi
-        if(!user){
-            throw Error('wrong')
-        }
-        const verify = await argon.verify(user.password, password)
-        if(!verify){
-            throw Error('wrong')
+        if (!user) {
+            throw new Error('Invalid email or password');
         }
 
-        // penggunaan jwt
+        const verify = await argon.verify(user.password, password);
+        if (!verify) {
+            throw new Error('Invalid email or password');
+        }
+
         const payload = {
             id: user.id,
-            role: user.role
-        }
-        const token = jwt.sign(payload, process.env.APP_SECRET || 'secretkey')
+            role: user.role,
+        };
+
+        const token = jwt.sign(payload, process.env.APP_SECRET || 'secretkey');
         return res.json({
             success: true,
-            message: 'login success',
+            message: 'Login success',
             results: {
-                token
-            }
-        })
-    }catch(err){
-        if(err.message === 'wrong'){
+                token,
+            },
+        });
+    } catch (err) {
+        if (err.message === 'Invalid email or password') {
             return res.status(401).json({
                 success: false,
-                message: 'wrong email or password'
-            })
+                message: 'Invalid email or password',
+            });
         }
-        console.log(err)
+
+        console.log(err);
         return res.status(500).json({
             success: false,
-            message: 'internal server error'
-        })
+            message: 'Internal server error',
+        });
     }
+};
 
-    // const {username, password} = req.body
-    // if(username === 'wisnu123' && password === 'wisnu123'){
-    //     return res.json({
-    //         success: true,
-    //         message: 'login success'
-    //     })
-    // }else{
-    //     return res.json({
-    //         success: false,
-    //         message: 'wrong username or password'
-    //     })
-    // }
-}
+exports.register = async (req, res) => {
+    try {
+        const { fullName, email, password, phoneNumber, role } = req.body;
 
-exports.register = async (req, res)=>{
-    try{
-        const {fullName, email, password, phoneNumber, role} = req.body
-        
-        const hashed = await argon.hash(password)
-        
-        const user =  userModel.create({
+        // Check if user with the same email already exists
+        const existingUser = await userModel.findOneByEmail(email);
+        if (existingUser) {
+            return res.status(409).json({
+                success: false,
+                message: 'Email has already been registered.',
+            });
+        }
+
+        const hashed = await argon.hash(password);
+        const user = await userModel.create({
             fullName,
             email,
             password: hashed,
             phoneNumber,
-            role
-        })
+            role,
+        });
+
         return res.json({
             success: true,
-            message: 'register success'
-        })
-    }catch(err){
-        console.log(err)
+            message: 'Registration success',
+        });
+    } catch (err) {
+        console.log(err);
         return res.status(500).json({
             success: false,
-            message: 'internal server error'
-        })
+            message: 'Internal server error',
+        });
     }
-}
+};
